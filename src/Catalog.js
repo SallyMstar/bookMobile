b782facdff465b9b90e0b5367361e8d7aa8a5afe
Sally Morningstar <sally.morningstar@icloud.com>
2019-02-10 16:04:11 -0500

    Bookmobile description & link update

--- src/Catalog.js
+++ src/Catalog.js
@@ -85,9 +85,11 @@ class Catalog extends Component {
                 <p>
 					<a href="https://pngtree.com/free-vectors"> free blank book image vector from pngtree.com</a>
           		</p>
-              </ol>
-								Available search terms for this sample app:
+							<p>
+								<strong>Available search terms for this sample app:</strong>
 								'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'
+								</p>
+              </ol>
             </div>
       </div>
 
